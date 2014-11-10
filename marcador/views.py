0ca7a7553d6be88e691d82ab0c1490b61d20de78
Hayri <hayri@hoshilab.com>
2014-11-10 21:47:41 +0100

    added views for edit and creating bookmarks

--- marcador/views.py
+++ marcador/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import BookmarkForm
 from .models import Bookmark
 
 def bookmark_list(request):
@@ -17,3 +20,29 @@ def bookmark_user(request, username):
   context = {'bookmarks': bookmarks, 'owner': user}
   return render(request, 'marcador/bookmark_user.html', context)
 
+@login_required
+def bookmark_create(request):
+  if request.method == 'POST':
+    form = BookmarkForm(data=request.POST)
+    if form.is_valid():
+      form.save(owner=request.user)
+      return redirect('marcador_bookmark_user', username=request.user.username)
+    else:
+      form = BookmarkForm()
+    return render(request, 'marcador/form.html',
+        {'form': form, 'create': True})
+
+@login_required
+def bookmark_edit(request, pk):
+  bookmark = get_object_or_404(Bookmark, pk=pk)
+  if bookmark.owner != request.user and not request.user.is_superuser:
+    raise PermissionDenied
+  if request.method == 'POST':
+    form = BookmarkForm(instance=bookmark, data=request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('marcador_bookmark_user', username=request.user.username)
+  else:
+    form = BookmarkForm(instance=bookmark)
+  return render(request, 'marcador/form.html', 
+      {'form': form, 'create': False, 'bookmark': bookmark})
